d0ed43cecf5024f7836689e98c53329f75650c49
SalehKhaled7 <lost1994soul@gmail.com>
2020-12-01 21:52:18 +0300

    BDO sql (insert, list, delete)

--- forms.php
+++ forms.php
@@ -1,69 +1,79 @@
 <html>
 <body>
+<h3>check database connection</h3>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
 
-<h3>save form</h3>
+try {
+    $db = new PDO("mysql:host=$servername;dbname=hotel_reservation", $username, $password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+
+
+<h3>show form</h3>
 <form action="?func=save" method="post">
     <!--    action is the place that we sending the date to ex: action="welcome.php"
         if we didn't put any thing it will send the data to the same page -->
-
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
-    <input type="submit">
-
-</form>
-
-<h3>show form</h3>
-<form action="?func=show" method="post">
-    <!--    action is the place that we sending the date to ex: action="welcome.php"
-        if we didn't put any thing it will send the data to the same page -->
-
     Name: <input type="text" name="name" required><br>
-    <!--    use required for not empty-->
-    E-mail: <input type="email" name="email" ><br>
-    <!--    use type="email" for email only filed-->
-    dob:<input type="number" name="dob" min="1990" max="2020"><br>
-    Gender:<input type="radio" name="gender" value="male">male<input type="radio" name="gender"
-                                                                     value="female">female<br>
-    <!--    select one of them only the name should be the same-->
-    Fav-color:
-    <input type="checkbox" name="color" value="red">red
-    <input type="checkbox" name="color" value="red">green
-    <input type="checkbox" name="color" value="red">blue
-    <br>
-    City:
-    <select name="city">
-        <option>ankara</option>
-        <option>istanbul</option>
-        <option>karabuk</option>
-    </select>
-    <br>
-    Description:
-    <br><textarea name="description" cols="30" rows="10"></textarea>
-    <br>
+    Star:<input type="number" name="star" min="1" max="5"><br>
+    City: <input type="text" name="city" required><br>
     <input type="submit">
-
 </form>
+
 
 <?php
-if ($_REQUEST["func"] == "show" and $_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"]; //get the data from the form with _POST[name]
-    $email = $_POST["email"];
-    $dob = $_POST["dob"];
-    $gender = $_POST["gender"];
-    $color = $_POST["color"];
+if (isset($_REQUEST["func"])  and $_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $star = $_POST["star"];
     $city = $_POST["city"];
-    $description = $_POST["description"];
+
     echo "name: " . $name, "<br>";
-    echo "email: " . $email, "<br>";
-    echo "dob: " . $dob, "<br>";
-    echo "gender: " . $gender, "<br>";
-    echo "fav-color: " . $color, "<br>";
+    echo "star: " . $star, "<br>";
     echo "city: " . $city, "<br>";
-    echo "description: " . $description, "<br>";
+
 }
-
-
 ?>
+
+<?php
+//add to data base
+if (isset($_REQUEST["func"])=='save'  and $_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $star = $_POST["star"];
+    $city = $_POST["city"];
+    $sql="INSERT INTO hotel (name,star,city) VALUES ('$name',$star,'$city')";
+    $db->exec($sql);
+    echo "New record created successfully";
+
+}
+?>
+<?php
+//list to data base
+    $sql = "SELECT * FROM hotel";
+    foreach ($db->query($sql) as $row){
+        echo $row['name']," ",$row['star']," ",$row['city'],"<br>";
+
+
+}
+?>
+<?php
+//delete from data base
+if (isset($_REQUEST["func"])=='delete'  and $_SERVER["REQUEST_METHOD"] == "POST") {
+    $id=$_REQUEST['id'];
+    $sql="DELETE FROM hotel WHERE id=$id";
+    $db->exec($sql);
+    header("location: ?func=delete");
+
+}
+?>
+
 
 </body>
 </html>
